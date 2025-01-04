a7692d7be17751eb4786f8cae7c215e6499c96a4
trsimanto <simanto14feb@gmail.com>
2025-01-05 02:23:09 +0600

    feat(login): login UI Complete

---
+++ lib/core/config/theme/color.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //BrandColor
+  static Color brandPink6D = const Color(0xffDF146D);
+  static Color whiteFF = const Color(0xffFFFFFF);
+
+
+
+}
---
+++ lib/core/values/icons/tr_icons.dart
@@ -0,0 +1,8 @@
+Map<String, String> trIconsSvg = {
+  "email": "assets/icons/email.svg",
+};
+
+
+Map<String, String> trIconsImg = {
+  "topLogo": "assets/images/toplogo.png",
+};
